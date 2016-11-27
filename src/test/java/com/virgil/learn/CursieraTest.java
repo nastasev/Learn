20a73e6d5cbe9e4d44ce36e4d725fe6ddd75cbef
Virgil <admin@cuc.ro>
2016-11-27 15:41:56 +0200

    added test to git

---
+++ src/test/java/com/virgil/learn/CursieraTest.java
@@ -0,0 +1,21 @@
+package com.virgil.learn;
+
+import com.virgil.learn.echipament.classes.biciclete.Cursiera;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class CursieraTest {
+    @Test
+    public void testGetSet() {
+        Cursiera cursiera = new Cursiera();
+
+        cursiera.schimbaPinion(1);
+        Assert.assertEquals(1,cursiera.getPinion());
+
+        cursiera.schimbaFoaie(3);
+        Assert.assertEquals(3,cursiera.getFoaie());
+
+        cursiera.schimbaFrana(2);
+        Assert.assertEquals(2,cursiera.getFrana());
+    }
+}
